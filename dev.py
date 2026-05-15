430ec30081b73e981971c4641457be8f0bbb2418
Krishnaswamy90 <krishnachitra21@gmail.com>
2026-05-15 17:09:22 +0530

    Number Guessing Game

--- dev.py
+++ dev.py
@@ -1,42 +1,31 @@
+import random
 
-class OverdraftError(Exception):
-    pass
+def guessing_number():
+  print("Welcome to the Number Guessing Number")
+  print("I am thinking of a number between 1 and 100")
 
-class Account:
-    def __init__(self,owner,initial_balance=0):
-        self.owner=owner
-        self.__balance=initial_balance
-        self.transaction_history=[]
+  secret_number=random.randint(1,100)
+  attempts=0
 
-    def deposit(self,amount):
-        if amount>0:
-            self.__balance+=amount
-            self.transaction_history.append(f"Deposited: ${amount}")
-            print(f"Successfully deposited ${amount}")
-        else:
-            print("Deposit amount be positive")
-    def withdraw(self,amount):
-        try:
-            if amount>self.__balance:
-                raise OverdraftError(f"Insufficient funds! Current balance: $ {self.__balance}")
-            if amount<=0:
-                print("Withdraw must be positive")
-                return
-            self.__balance-=amount
-            self.transaction_history.append(f"Withdrew: ${amount}")
+  while True:
+    guess=input("Take a guess")
 
-        except OverdraftError as e:
-            print(f"Error: {e}")
+    if not guess.isdigit():
+      print("Please enter a valid number")
+      continue
 
-    def get_balance(self):
-        return f"Account balance for {self.owner}: ${self.__balance}"
-    def show_history(self):
-        print(f"\n--- Transaction History for {self.owner}---")
-        for record in self.transaction_history:
-            print(record)
-my_acc=Account("Krishnaswamy", 500)
-print(my_acc.get_balance())
-my_acc.deposit(200)
-my_acc.withdraw(100)
-my_acc.withdraw(1000)
-my_acc.show_history()
+    guess=int(guess)
+    attempts+=1
+
+
+    if guess<secret_number:
+      print("Too Low")
+    elif guess>secret_number:
+      print("Too High")
+    else:
+      print(f"Congrats you have found {attempts} attempts")
+
+    
+if __name__=="__main__":
+  guessing_number()
+  
